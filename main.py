5870a0b2b81045b0d50a0e7b88a5a240fd0fd826
mikhalexandr <mikhalchenkov22@gmail.com>
2025-03-20 05:44:44 +0500

    commit

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from starlette import status
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.types import ASGIApp
 
 from core.settings import settings
 from core.logger import init_logger, logger
@@ -10,6 +15,21 @@ from core.postgres.initialization import PostgresClient
 from core.redis.initialization import RedisClient
 from core.roboflow.initialization import RoboflowClient
 from services import init_routers
+
+
+class LimitUploadSize(BaseHTTPMiddleware):
+    def __init__(self, app: ASGIApp, max_upload_size: int) -> None:
+        super().__init__(app)
+        self.max_upload_size = max_upload_size
+
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        if request.method == 'POST':
+            if 'content-length' not in request.headers:
+                return Response(status_code=status.HTTP_411_LENGTH_REQUIRED)
+            content_length = int(request.headers['content-length'])
+            if content_length > self.max_upload_size:
+                return Response(status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE)
+        return await call_next(request)
 
 
 @asynccontextmanager
@@ -45,6 +65,7 @@ def init_app() -> FastAPI:
         allow_methods=["*"],
         allow_headers=["*"],
     )
+    _app.add_middleware(LimitUploadSize, max_upload_size=300_000_000)
     init_routers(_app)
     return _app
 
